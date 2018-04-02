a4a275c5f1e29638c3136c6361396c555589dfb4
Stanislav <kapinuss@rambler.ru>
2018-04-02 16:33:35 +0300

    tired bug

--- src/main/scala/JDBC.scala
+++ src/main/scala/JDBC.scala
@@ -1,23 +1,25 @@
 import scalikejdbc._
+import JDBCSettings._
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.stream.ActorMaterializer
+
+case class Arbeitgeber(title: String)
 
 object JDBC {
-
-  GlobalSettings.loggingSQLAndTime = new LoggingSQLAndTimeSettings(
-    enabled = true,
-    singleLineMode = true,
-    logLevel = 'DEBUG
-  )
-
-  Class.forName("org.postgresql.Driver")
-  val poolSettings = new ConnectionPoolSettings(initialSize = 100, maxSize = 100)
-  val url = "jdbc:postgresql://localhost:5432/postgres"
-  val user = "postgres"
-  val password = "postgres"
 
   ConnectionPool.singleton(url, user, password, poolSettings)
 
   def main(args: Array[String]): Unit = {
+
+    implicit val system: ActorSystem = ActorSystem()
+    implicit val mat: ActorMaterializer = ActorMaterializer()
+
     implicit val session: AutoSession.type = AutoSession
-    sql"""create table test (id serial not null primary key, name varchar(64), created_at timestamp not null)""".execute.apply()
+
+    sql"""CREATE TABLE IF NOT EXISTS arbeitgeber (id serial not null primary key, title varchar(64), created_at timestamp not null)""".execute.apply()
+
+    val insertActor: ActorRef = system.actorOf(Props[InsertActor], "insertActor")
+    insertActor ! Arbeitgeber("Sbertech")
+
   }
 }
